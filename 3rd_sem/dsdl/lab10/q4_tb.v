70631f20b153c55fd75aeff6729458d00d4146f0
Prerna Mittal <93262556+prernamittal@users.noreply.github.com>
2022-11-17 11:18:26 +0530

    Update q4_tb.v

--- 3rd_sem/dsdl/lab10/q4_tb.v
+++ 3rd_sem/dsdl/lab10/q4_tb.v
@@ -5,13 +5,14 @@ reg [4:0]in;
 reg clk,reset;
 wire [4:0]out;
 q4 ques(in,out,clk,reset);
+always #5 clk= (~clk);
+initial begin
+	clk=0;
+	#150 $finish;
+end
 initial begin
 	$dumpfile("q4_tb.vcd");
 	$dumpvars(0,q4_tb);
-	clk=0;
-	forever #20 clk= (~clk);
-end
-initial begin
 	in=5'd1; reset=1;#20;
     	in=5'd10; reset=0;#20;
     	in=5'd5; reset=1;#20;
