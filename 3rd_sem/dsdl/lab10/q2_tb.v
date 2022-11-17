9602ac729ebb397b6fac8cf1172b67465f8320dc
Prerna Mittal <93262556+prernamittal@users.noreply.github.com>
2022-11-17 11:15:36 +0530

    Update q2_tb.v

--- 3rd_sem/dsdl/lab10/q2_tb.v
+++ 3rd_sem/dsdl/lab10/q2_tb.v
@@ -4,13 +4,14 @@ module q2_tb();
 reg t,clk,reset;
 wire q;
 q2 ques(t,clk,reset,q);
+always #5 clk= (~clk);
+initial begin
+	clk=0;
+	#150 $finish;
+end
 initial begin
 	$dumpfile("q2_tb.vcd");
 	$dumpvars(0,q2_tb);
-	clk=0;
-	forever #20 clk= (~clk);
-end
-initial begin
 	t=1; reset=1;#20;
     	t=1; reset=0;#20;
     	t=0; reset=1;#20;
