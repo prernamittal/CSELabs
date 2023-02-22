2f529338086f73757700bf9afd496141454b7d6c
prernamittal <prernaharshi@hotmail.com>
2023-02-22 16:09:27 +0530

    dbsl lab4 done

--- Sem4/al/Lab2&3-Additional/Ques4-RecursivePartition.c
+++ Sem4/al/Lab2&3-Additional/Ques4-RecursivePartition.c
@@ -1,48 +1,36 @@
-#include <stdio.h>>
+#include <stdbool.h>
+#include <stdio.h>
 
-using namespace std;
-
-bool subsetSumUtil(int ind, int target, int arr[], arr dp[][]){
-    if(target==0)
-        return true;
-    
-    if(ind == 0)
-        return arr[0] == target;
-    
-    if(dp[ind][target]!=-1)
-        return dp[ind][target];
-        
-    bool notTaken = subsetSumUtil(ind-1,target,arr,dp);
-    
-    bool taken = false;
-    if(arr[ind]<=target)
-        taken = subsetSumUtil(ind-1,target-arr[ind],arr,dp);
-        
-    return dp[ind][target]= notTaken||taken;
+bool isSubsetSum(int arr[], int n, int sum){
+	if (sum == 0)
+		return true;
+	if (n == 0 && sum != 0)
+		return false;
+	if (arr[n - 1] > sum)
+		return isSubsetSum(arr, n - 1, sum);
+	return isSubsetSum(arr, n - 1, sum) || isSubsetSum(arr, n - 1, sum - arr[n - 1]);
 }
 
-bool canPartition(int n, vector<int> &arr){
-    
-    int totSum=0;
-    
-    for(int i=0; i<n;i++){
-        totSum+= arr[i];
+bool partition(int arr[], int s){
+	int sum = 0;
+	for (int i = 0; i < s; i++)
+		sum += arr[i];
+	if (sum % 2 != 0)
+		return false;
+	return isSubsetSum(arr, s, sum / 2);
+}
+
+int main(){
+	int n, arr[10];
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    printf("Enter array elements: ");
+    for(int i=0;i<n;i++){
+        scanf("%d", &arr[i]);
     }
-    
-    if (totSum%2==1) return false;
-    
-    else{
-        int k = totSum/2;
-        vector<vector<int>> dp[n,arr[k+1,-1]];
-        return subsetSumUtil(n-1,k,arr,dp);
-    } 
-}
-
-int main() {
-  int arr = {2,3,3,3,4,5};
-  int n = arr.size();    
-  if(canPartition(n,arr))
-    printf("The Array can be partitioned into two equal subsets");
-  else 
-    printf("The Array cannot be partitioned into two equal subsets");
+	if (partition(arr, n) == true)
+		printf("\nCan be divided into two subsets of equal sum\n");
+	else
+		printf("\nCan not be divided into two subsets of equal sum\n");
+	return 0;
 }
