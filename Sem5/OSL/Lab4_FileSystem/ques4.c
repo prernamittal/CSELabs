aa61e48cb5e4f824e2967bedaf16daeaeab194f1
prernamittal <prernaharshi@hotmail.com>
2023-08-29 21:31:11 +0530

    os lab 4. ques3,4 added

---
+++ Sem5/OSL/Lab4_FileSystem/ques4.c
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]){
+	char *oldpath = argv[1];
+	char *newpath = "newpath.txt";
+	struct stat sb;
+	if (argc < 2) {
+		printf ("insufficient arguments");
+		return 0;
+	}
+	int ret1 = stat (oldpath, &sb);
+	if (ret1) {
+		perror ("stat");
+		return 1;
+	}
+	printf("Before linking: %ld\n", sb.st_nlink);
+	symlink(oldpath, newpath);
+	printf("After linking: \n");
+	system("ls");
+	int ret2 = stat(newpath, &sb);
+	if(ret2 != 0){
+		perror("stat");
+		return 1;
+	}
+	printf("After linking: %ld\nNew path: %s\n", sb.st_nlink, newpath);
+	unlink(oldpath);
+	printf("After unlinking: \n");
+	system("ls");
+	int ret3 = stat(newpath, &sb);
+	if(ret3 != 0){
+		perror("stat");
+		return 1;
+	}
+	printf("After unlinking: %ld\n", sb.st_nlink);	
+	return 0;
+}
